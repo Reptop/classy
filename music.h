05681dce2c144b7152d02055c20e751c7302ef2e
Ren <36342803+Reptop@users.noreply.github.com>
2021-12-08 11:01:32 -0800

    Update music.h

--- music.h
+++ music.h
@@ -9,12 +9,13 @@ using namespace std;
 
 class Music : public Media {
  public:
-  Music();
-    virtual int getType();
+  Music(); //contructor 
+    virtual int getType(); //virtual function for assiging music child a type. Used for dynamic casting in main.cpp 
     char* getArtist();
     char* getPublisher();
     int* getDuration();
  private:
+    //private variables for user to store information regarding music 
     char artist[40];
     char publisher[40];
     int duration;
